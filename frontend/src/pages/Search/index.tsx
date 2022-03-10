d04c9dbb4b78c2c50ca7e44222e734de625b03b4
Davi Paim Rios <davipaimrios@gmail.com>
2022-03-10 10:55:35 -0300

    FRONT - Add person item sorter

--- frontend/src/pages/Search/index.tsx
+++ frontend/src/pages/Search/index.tsx
@@ -8,40 +8,62 @@ import PersonRequest from '$api/requests/Person'
 import Button from '$components/Button'
 import PersonView from './PersonView'
 
+type SortOrder = 'asc' | 'desc'
+
+const personSorter = (field: keyof PersonData = 'name', order: SortOrder = 'asc') => {
+  return (a: PersonData, b: PersonData) => {
+    const fieldA = a[field]
+    const fieldB = b[field]
+
+    const result = order === 'asc' ? 1 : -1
+    if(fieldA === undefined || fieldB === undefined) return result
+
+    if (typeof fieldA === 'number'){
+      return (fieldA - Number(fieldB)) * result
+    }
+    if (typeof fieldA === 'string'){
+      return result > 0
+        ? fieldA.localeCompare(String(fieldB))
+        : String(fieldB).localeCompare(fieldA)
+    }
+    return 0
+  }
+}
+
 const Search = () => {
   const [persons, setPersons] = useState<PersonData[]>([
     {
-      name: 'Nome1',
-      birthday: Date.now(),
+      name: 'José',
+      birthday: new Date(1983, 4, 21).getTime(),
       email: 'email@email.com',
-      id: 192837,
+      id: 4,
       phone_number: '7198765321',
       city: 'Salvador',
       country: 'Brasil'
     },
     {
-      name: 'Nome2',
-      birthday: Date.now(),
+      name: 'Dario',
+      birthday: new Date(1973, 8, 2).getTime(),
       email: 'email@email.com',
-      id: 21345,
+      id: 3,
       phone_number: '7198765321',
       city: 'Salvador',
       country: 'Brasil'
     },
     {
-      name: 'Nome3',
-      birthday: Date.now(),
+      name: 'Rafaela',
+      birthday: new Date(1999, 1, 28).getTime(),
       email: 'email@email.com',
-      id: 67854,
+      id: 1,
       phone_number: '7198765321',
       city: 'Salvador',
       country: 'Brasil'
     },
     {
-      name: 'Nome4',
-      birthday: Date.now(),
+      name: 'Júlia',
+      birthday: new Date(1960, 12, 25).getTime(),
       email: 'email@email.com',
-      id: 45173,
+      id: 2,
       phone_number: '7198765321',
       city: 'Salvador',
       country: 'Brasil'
@@ -50,13 +72,18 @@ const Search = () => {
 
   const fetchPersons = useCallback(async () => {
     const { status, data: { message, data } } = await PersonRequest.index()
-    if(status === 200 && data) setPersons(data)
+    if(status === 200 && data) {
+      setPersons(data)
+    }
     console.log(message)
   }, [persons])
 
   useLayoutEffect(() => {
     if(persons.length < 1)
       fetchPersons()
+      
+    const p = [ ...persons ]
+    setPersons(p.sort(personSorter('name', 'asc')))
   }, [])
 
   /*
@@ -76,16 +103,31 @@ const Search = () => {
     setShowPersonDetails(true)
   }
 
+  const [fieldSorted, setFieldSorted] = useState<[keyof PersonData, SortOrder]>(['name', 'asc'])
+  const sortPersons = (field?: keyof PersonData) => {
+    const p = [ ...persons ]
+
+    let order: SortOrder = 'asc'
+    if(field === fieldSorted[0]) {
+      order = 'asc' === fieldSorted[1] ? 'desc' : 'asc'
+    }
+
+    setPersons(p.sort(personSorter(field, order)))
+    setFieldSorted([field || 'name', order])
+  }
+
   return (
-    <main>
+    <main className={styles.searchPage}>
       <article>
-        <input placeholder='Pesquisa...'
-          value={''}
-          onChange={() => {}}
-        />
-        <Button onClick={() => fetchPersons()}>
-          Recarregar
-        </Button>
+        <div className={styles.search}>
+          <input placeholder='Pesquisa...'
+            value={''}
+            onChange={() => {}}
+          />
+          <Button onClick={() => fetchPersons()}>
+            Recarregar
+          </Button>
+        </div>
         <table className={styles.table}>
           <colgroup>
             <col />
@@ -94,8 +136,12 @@ const Search = () => {
           </colgroup>
           <thead>
             <tr>
-              <th>Nome</th>
-              <th>Data de Nascimento</th>
+              <th onClick={() => sortPersons('name')}>
+                Nome {fieldSorted[0] === 'name' ? fieldSorted[1] === 'asc' ? 'V' : '^' : '' }
+              </th>
+              <th onClick={() => sortPersons('birthday')}>
+                Data de Nascimento {fieldSorted[0] === 'birthday' ? fieldSorted[1] === 'asc' ? 'V' : '^' : '' }
+              </th>
               <th></th>
             </tr>
           </thead>
@@ -109,6 +155,11 @@ const Search = () => {
             })}
           </tbody>
         </table>
+        <div className={styles.pageSelectorContainer}>
+          <button onClick={() => {}}>{'<'}</button>
+          <p>{10} - {20}</p>
+          <button>{'>'}</button>
+        </div>
       </article>
       <section className={styles.personViewContainer} style={{ display: showPersonDetails ? '' : 'none' }}>
         <article>
