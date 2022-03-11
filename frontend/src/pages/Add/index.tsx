794d77ab0514e10c505b905aeb35234b814290d9
Davi Paim Rios <davipaimrios@gmail.com>
2022-03-11 18:55:41 -0300

    FRONT - Add create person form

--- frontend/src/pages/Add/index.tsx
+++ frontend/src/pages/Add/index.tsx
@@ -1,10 +1,104 @@
-import PersonForm from '$components/PersonForm'
+import { FormEvent, useState } from 'react'
+import styles from './index.module.sass'
+
+import PersonRequest from '$api/requests/Person'
+import Button from '$components/Button'
+
+const dateStringToNumber = (date: string) => {
+  const [year, month, day] = date.split('-')
+  const newDate = new Date(Number(year), Number(month), Number(day))
+  const timestamp = newDate.getTime()
+  return timestamp
+}
+
+const dateNumberToString = (date: number) => {
+  const dateObject = new Date(date)
+  const [year, month, day] = [ dateObject.getFullYear(), dateObject.getMonth(), dateObject.getDate() ]
+  const dateString = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`
+  return dateString
+}
 
 const Add = () => {
+  const [name, setName] = useState('')
+  const [birthday, setBirthday] = useState(Date.now())
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [country, setCountry] = useState('')
+  const [city, setCity] = useState('')
+
+  const createPerson = async (event: FormEvent) => {
+    event.preventDefault()
+    const response = await PersonRequest.create({ name, birthday, phone_number: phoneNumber, email, country, city })
+  }
+
+  const reset = (event: FormEvent) => {
+    event.preventDefault()
+    setName('')
+    setBirthday(Date.now())
+    setPhoneNumber('')
+    setEmail('')
+    setCountry('')
+    setCity('')
+  }
+
   return (
-    <main>
+    <main className={styles.add}>
       <article>
-        <PersonForm/>
+        <form onSubmit={createPerson} onReset={reset}>
+          <label>
+            Nome
+            <input placeholder='Nome da pessoa'
+              type='text'
+              value={name}
+              onChange={(event) => setName(event.currentTarget.value)}
+            />
+          </label>
+          <label>
+            Data de nascimento
+            <input placeholder='2020-05-25'
+              type='date'
+              value={dateNumberToString(birthday)}
+              onChange={(event) => setBirthday(dateStringToNumber(event.currentTarget.value))}
+            />
+          </label>
+          <label>
+            Telefone
+            <input placeholder='71987654321'
+              type='tel'
+              value={phoneNumber}
+              pattern='[1-9]{2}9[0-9]{8}'
+              onChange={(event) => setPhoneNumber(event.currentTarget.value)}
+            />
+          </label>
+          <label>
+            Email
+            <input placeholder='email@email.com'
+              type='email'
+              value={email}
+              onChange={(event) => setEmail(event.currentTarget.value)}
+            />
+          </label>
+          <label>
+            País
+            <input placeholder='Brasil'
+              type='text'
+              value={country}
+              onChange={(event) => setCountry(event.currentTarget.value)}
+            />
+          </label>
+          <label>
+            Cidade
+            <input placeholder='Salvador'
+              type='text'
+              value={city}
+              onChange={(event) => setCity(event.currentTarget.value)}
+            />
+          </label>
+          <div>
+            <Button type='submit' theme='confirm'>Cadastrar Pessoa</Button>
+            <Button type='reset' theme='warning'>Limpar</Button>
+          </div>
+        </form>
       </article>
     </main>
   )
